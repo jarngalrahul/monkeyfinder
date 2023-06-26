b72d1d434f82a3b48919bc078a8ec3dd2c71e98d
jarngalrahul <rahuljarngal28@gmail.com>
2023-06-26 18:52:17 +0530

    added emptyViews, and dark mode

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -18,6 +18,9 @@ public partial class MonkeysViewModel : BaseViewModel
         this.geolocation = geolocation;
     }
 
+    [ObservableProperty]
+    bool isRefreshing;
+
     [RelayCommand]
     async Task GetClosestMonkeyAsync()
     {
@@ -100,6 +103,7 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;
+            IsRefreshing = false;
         }
     }
 }
